529af8b3de9f69fcf4bcb4996eb990ca7604bf28
AZhaoTongXue <2223757448@qq.com>
2023-10-19 00:42:01 +0800

    practice_1: print, for-in, if-else, list

--- practice_1.py
+++ practice_1.py
@@ -1,2 +1,27 @@
 
 print('Hello World')
+
+fruit_list = ['apple', 'banana', 'peach']
+
+for fruit in fruit_list:
+    print(fruit)
+
+import random
+
+num_list = []
+
+for i in range(0, 9):
+    num = random.randint(1, 10)
+    num_list += [num]
+
+print(num_list)
+
+is_apple = False
+
+for fruit in fruit_list:
+    if fruit == 'apple':
+        is_apple = True
+    else:
+        is_apple = False
+    print(is_apple)
+
