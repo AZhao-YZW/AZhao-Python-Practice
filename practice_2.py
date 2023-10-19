28d2b3b18efd87e9917ce6beab5224ff1ea525e8
AZhaoTongXue <2223757448@qq.com>
2023-10-20 01:32:23 +0800

    practice_2: turtle, ipython, IDLE

---
+++ practice_2.py
@@ -0,0 +1,15 @@
+'''
+官方教程: https://docs.python.org/zh-cn/3/library/turtle.html
+
+注：教程中用python命令行交互方式学习turtle较为方便，由于原生python交互不支持
+自动补全，安装ipython交互式shell提高效率，此外ipython还支持bash shell命令。
+
+不建议用python自带开发环境IDLE。
+'''
+
+
+from turtle import *
+
+setup()
+
+done()
