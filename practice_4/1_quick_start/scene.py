368ae0d60f84ed19ea04661e7c89b6eb9e7e9e14
AZhaoTongXue <2223757448@qq.com>
2023-11-13 00:25:02 +0800

    practice_4: 学习manim库的community版本官方教程

---
+++ practice_4/1_quick_start/scene.py
@@ -0,0 +1,166 @@
+'''
+manim官方教程: https://docs.manim.community/en/stable/tutorials
+
+practice_4使用Anaconda所创建的虚拟环境`manim`, Python版本3.12.0
+
+1. Python格式化工具选择autopep8, 需在虚拟环境中下载`pip install autopep8`
+2. manim命令加上`-v WARNING`参数可以过滤生成视频时的INFO打印
+
+
+'''
+
+from manim import *
+
+'''quick start'''
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        # interpolate the square into the circle
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))  # fade out animation
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+
+
+'''building blocks'''
+
+
+class CreatingMobjects(Scene):
+    def construct(self):
+        circle = Circle()
+        self.add(circle)
+        self.wait(1)
+        self.remove(circle)
+        self.wait(1)
+
+
+class MobjectPlacement(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        # place the circle two units left from the origin
+        circle.move_to(LEFT * 2)
+        # place the square to the left of the circle
+        square.next_to(circle, LEFT)
+        # align the left border of the triangle to the left border of the circle
+        triangle.align_to(circle, LEFT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+
+class MobjectStyling(Scene):
+    def construct(self):
+        circle = Circle().shift(LEFT)
+        square = Square().shift(UP)
+        triangle = Triangle().shift(RIGHT)
+
+        circle.set_stroke(color=GREEN, width=20)
+        square.set_fill(YELLOW, opacity=1.0)
+        triangle.set_fill(PINK, opacity=0.5)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+
+class MobjectZOrder(Scene):
+    def construct(self):
+        circle = Circle().shift(LEFT)
+        square = Square().shift(UP)
+        triangle = Triangle().shift(RIGHT)
+
+        circle.set_stroke(color=GREEN, width=20)
+        square.set_fill(YELLOW, opacity=1.0)
+        triangle.set_fill(PINK, opacity=0.5)
+
+        self.add(triangle, square, circle)
+        self.wait(1)
+
+
+class SomeAnimations(Scene):
+    def construct(self):
+        square = Square()
+
+        # some animations display mobjects, ...
+        self.play(FadeIn(square))
+
+        # ... some move or rotate mobjects around...
+        self.play(Rotate(square, PI/4))
+
+        # some animations remove mobjects from the screen
+        self.play(FadeOut(square))
+
+        self.wait(1)
+
+
+class AnimateExample(Scene):
+    def construct(self):
+        square = Square().set_fill(RED, opacity=1.0)
+        self.add(square)
+
+        # animate the change of color
+        self.play(square.animate.set_fill(WHITE))
+        self.wait(1)
+
+        # animate the change of position and the rotation at the same time
+        self.play(square.animate.shift(UP).rotate(PI / 3))
+        self.wait(1)
+
+class RunTime(Scene):
+    def construct(self):
+        square = Square()
+        self.add(square)
+        self.play(square.animate.shift(UP), run_time=3)
+        self.wait(1)
