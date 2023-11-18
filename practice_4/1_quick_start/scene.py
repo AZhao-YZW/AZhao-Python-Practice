9d54c5977c543cd72dbae0b5ba83932a1bd33d87
AZhaoTongXue <2223757448@qq.com>
2023-11-18 12:59:52 +0800

    practice_4: 学习manim库的community版本官方教程

--- practice_4/1_quick_start/scene.py
+++ practice_4/1_quick_start/scene.py
@@ -158,9 +158,41 @@ class AnimateExample(Scene):
         self.play(square.animate.shift(UP).rotate(PI / 3))
         self.wait(1)
 
+
 class RunTime(Scene):
     def construct(self):
         square = Square()
         self.add(square)
         self.play(square.animate.shift(UP), run_time=3)
         self.wait(1)
+
+
+class Count(Animation):
+    def __init__(self, number: DecimalNumber, start: float, end: float, **kwargs) -> None:
+        # Pass number as the mobject of the animation
+        super().__init__(number,  **kwargs)
+        # Set start and end
+        self.start = start
+        self.end = end
+
+    def interpolate_mobject(self, alpha: float) -> None:
+        # Set value of DecimalNumber according to alpha
+        value = self.start + (alpha * (self.end - self.start))
+        self.mobject.set_value(value)
+
+
+class CountingScene(Scene):
+    def construct(self):
+        # Create Decimal Number and add it to scene
+        number = DecimalNumber().set_color(WHITE).scale(5)
+        # Add an updater to keep the DecimalNumber centered as its value changes
+        number.add_updater(lambda number: number.move_to(ORIGIN))
+
+        self.add(number)
+
+        self.wait()
+
+        # Play the Count Animation to count from 0 to 100 in 4 seconds
+        self.play(Count(number, 0, 100), run_time=4, rate_func=linear)
+
+        self.wait()
